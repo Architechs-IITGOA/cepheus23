0adef2a8806da88805952a8631cbd624ed9a5045
adarsh_nidamanuri <99207348+Adarsh-N123@users.noreply.github.com>
2022-10-23 21:32:08 +0530

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,35 +1,83 @@
 import React from "react";
 import "./About.css";
+import MediaQuery from 'react-responsive';
+import './index.css';
 
-export default function About() {
+function About() {
+  const myStyle = {
+    color: "white",
+    marginTop:"150px",
+    fontSize:"40px",
+    fontFamily:"Roboto Slab,serif",
+
+    
+  };
+
+  
+  const textStyle={
+    fontSize:"20.18px",
+    // fontSize: idleFontSize / fontScale,
+    color:"white",
+    margin:"40px",
+    fontFamily:"Roboto Slab,serif",
+    
+
+
+  }
+  const textStyle1={
+    fontSize:"11.18px",
+    // fontSize: idleFontSize / fontScale,
+    color:"white",
+    margin:"40px",
+    fontFamily:"Roboto Slab,serif",
+    
+
+
+  }
+  const textPara2={
+    fontSize:"20.18px",
+    // fontSize: idleFontSize / fontScale,
+    color:"white",
+    margin:"40px",
+    fontFamily:"Roboto Slab,serif",
+  }
+  const textPara1={
+    fontSize:"11.18px",
+    // fontSize: idleFontSize / fontScale,
+    color:"white",
+    margin:"40px",
+    fontFamily:"Roboto Slab,serif",
+  }
   return (
-    <>
-      <div className="about">
-        <div className="aboutContent">
-          <div className="aboutTitle">About Us</div>
-          <div className="aboutPara">
-            <p>
-              IIT Goa, only six years into its inception, has already grown to
-              be a highly sought-after institution for engineering aspirants
-              throughout the country, nestled into the vast and beautiful campus
-              of GEC in Farmagudi with a vibrant population of over three
-              thousand students and five hundred faculty members. With Cepheus,
-              we strive to achieve excellence in technical endeavours; however,
-              we are not untouched by the rich and unique culture of Goa, which
-              will be reflected in the fest’s various subtleties.
+    <div className="App">
+      <h1 style={myStyle}>About Us</h1>
+      <div >
+        <div className='box glowing'>
+        <MediaQuery minWidth={700}>
+          <p className='text' style={textStyle}>&nbsp;&nbsp;IIT Goa, only six years into its inception, has already grown to be a highly sought-after institution for engineering aspirants throughout the country, nestled into the vast and beautiful campus of GEC in Farmagudi with a vibrant population of over three thousand students and five hundred faculty members. With Cepheus, we strive to achieve excellence in technical endeavours; however, we are not untouched by the rich and unique culture of Goa, which will be reflected in the fest’s various subtleties.
             </p>
-            <p>
-              Cepheus is a celebration of our technical prowess - not just as an
-              institution, but as a country. It aims at instilling a sense of
-              collective learning by being a melting pot of ideas open to
-              students of various institutions and disciplines. With intriguing
-              events and competitions, along with resourceful workshops by
-              experts and professionals, we wish to make Cepheus ‘22 a memorable
-              experience for all concerned.
+        </MediaQuery>
+
+        <MediaQuery maxWidth={700}>
+          <p className='text' style={textStyle1}>&nbsp;&nbsp;IIT Goa, only six years into its inception, has already grown to be a highly sought-after institution for engineering aspirants throughout the country, nestled into the vast and beautiful campus of GEC in Farmagudi with a vibrant population of over three thousand students and five hundred faculty members. With Cepheus, we strive to achieve excellence in technical endeavours; however, we are not untouched by the rich and unique culture of Goa, which will be reflected in the fest’s various subtleties.
             </p>
-          </div>
+        </MediaQuery>
+        <MediaQuery minWidth={700}>
+            <p className='text' style={textPara2}>
+            &nbsp;&nbsp;Cepheus is a celebration of our technical prowess - not just as an institution, but as a country. It aims at instilling a sense of collective learning by being a melting pot of ideas open to students of various institutions and disciplines. With intriguing events and competitions, along with resourceful workshops by experts and professionals, we wish to make Cepheus ‘22 a memorable experience for all concerned.
+            </p>
+        </MediaQuery>
+        <MediaQuery maxWidth={700}>
+            <p className='text' style={textPara1}>
+            &nbsp;&nbsp;Cepheus is a celebration of our technical prowess - not just as an institution, but as a country. It aims at instilling a sense of collective learning by being a melting pot of ideas open to students of various institutions and disciplines. With intriguing events and competitions, along with resourceful workshops by experts and professionals, we wish to make Cepheus ‘22 a memorable experience for all concerned.
+            </p>
+        </MediaQuery>
         </div>
-      </div>
-    </>
+        
+        
+          
+        </div>
+    </div>
   );
 }
+export default About;
