cfcb06506c1be4cd85443630b26f2854357b3165
Kushagra Srivastava <kushagrasrivastava908@gmail.com>
2022-12-27 13:34:20 +0530

    Updating profile section and adding some profile functions

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -4,9 +4,10 @@ import "./Profile.css"
 export default function Profile({isProfileClicked}) {
     return (
         <div class={isProfileClicked ? "profile profile-active" : "profile"}>
+            <img src="" alt=""></img>
             <div class="profile-left">
-                <img src="avatars/21616081_100-01.png" alt="avatar"></img>
-                <img src="Used Elements-20221127T203156Z-001/28509144_vector_badge_element_set_dd_badge_elements_collections-03.png" alt=""></img>
+                <img src="img/avatar1.png" alt="avatar" class="profile-image"></img>
+                <img src="img/profile_name_border.png" alt="" class="name-border"></img>
                 {/* style="height: 150px;width: 500px;" */}
                 <p class="name"> Raghavendra Singh </p>
                 <p> Indian Institute of Technology (IIT), Goa </p>
@@ -16,7 +17,21 @@ export default function Profile({isProfileClicked}) {
                 {/* style="font-family: headerFont;" */}
             </div>
             <div className="profile-right">
-
+                <div class="participated-events">
+                    <h2>Participated Events</h2>
+                    <ol>
+                       <li>Treasure Hunt</li>
+                       <li>Beat The Street</li>
+                       <li>Hack Overflow</li>
+                       <li>Bridge Builder</li>
+                       <li>Front End Challange</li>
+                       <li>Play With Microbit</li>
+                    </ol>
+                </div>
+                <div className="profile-buttons">
+                    <a class="edit">Edit Profile</a>
+                    <a class="log-out">Log Out</a>
+                </div>
             </div>
         </div>
     )
--- src/sitecontainer.js
+++ src/sitecontainer.js
@@ -7,6 +7,7 @@ import Schedule from "./Components/Schedule/Schedule";
 import Sponsors from "./Components/Sponsors/Sponsors";
 import Contact from "./Components/Contact/Contact";
 import Profile from "./Components/Profile/Profile";
+import "./sitecontainer.css";
 
 const SiteContainer = () => {
 
@@ -15,16 +16,25 @@ const SiteContainer = () => {
   const handleProfileClick = () => {
     setProfileClicked(!isProfileClicked);
   }
+
+  const handleBodyClick = () => {
+    setProfileClicked(false);
+  }
+
   return (
-    <div >
-      <Navbar handleProfileClick = {handleProfileClick}/>
-      <Profile isProfileClicked = {isProfileClicked}/>
-      <Home />
-      <About />
-      <Events />
-      <Schedule />
-      <Sponsors />
-      <Contact />
+    <div>
+      <div>
+        <Navbar handleProfileClick = {handleProfileClick}/>
+        <Profile isProfileClicked = {isProfileClicked}/>
+      </div>
+      <div onClick={() => handleBodyClick()} class={isProfileClicked? "blur noblur" : "noblur"}>
+        <Home />
+        <About />
+        <Events />
+        <Schedule />
+        <Sponsors />
+        <Contact />
+      </div>
     </div>
   );
 };
