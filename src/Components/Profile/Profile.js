603d63bdcb0b06ddc12f400f20f7a44c54a8ca69
adarsh_nidamanuri <99207348+Adarsh-N123@users.noreply.github.com>
2022-12-30 23:52:47 +0530

    google authentication profile

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,57 +1,28 @@
-// import React from "react";
+
 import React, { Component,useEffect,useState } from 'react'
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import "./Profile.css"
 import { gapi } from 'gapi-script';
-// import GoogleBtn from './GoogleBtn'
-// import { useGoogleLogin } from '@react-oauth/google';
 
-export default function Profile({isProfileClicked}) {
+
+export default function Profile({isProfileClicked,auth_reset,auth_continue}) {
 
 
     const clientId = '433800136317-usopqpji8k1tu6u1pq1r2t689j091p65.apps.googleusercontent.com';
-    const [success,setsuccess] = useState(false);
-    const responseGoogle = (response) => {
-        console.log(response);
-        setsuccess(true);
-    }
-    const responseGoogle1 = (response) => {
-        console.log(response);
-        setsuccess(false);
-    }
-    useEffect(() => {
-        const initClient = () => {
-            gapi.client.init({
-            clientId: clientId,
-            scope: ''
-            });
-        };
-        gapi.load('client:auth2', initClient);
-    });
-
-    const onSuccess = (res) => {
-        console.log('success:', res);
-    };
-    const onFailure = (err) => {
-        console.log('failed:', err);
-    };
-    function setter(){
-        setsuccess(true);
-    }
-    // 
+    
     return (
         <div class={isProfileClicked ? "profile profile-active" : "profile"}>
             <img src="" alt=""></img>
             <div class="profile-left">
                 <img src="img/avatar1.png" alt="avatar" class="profile-image"></img>
                 <img src="img/profile_name_border.png" alt="" class="name-border"></img>
-                {/* style="height: 150px;width: 500px;" */}
+                
                 <p class="name"> Raghavendra Singh </p>
                 <p> Indian Institute of Technology (IIT), Goa </p>
                 <p> gstechnical@iitgoa.ac.in </p>
                 <p> 9785075997 </p>
                 <p class="score"> <span> Score </span> - 500 </p>
-                {/* style="font-family: headerFont;" */}
+                
             </div>
             <div className="profile-right">
                 <div class="participated-events">
@@ -66,27 +37,10 @@ export default function Profile({isProfileClicked}) {
                     </ol>
                 </div>
                 <div className="profile-buttons">
-                    {/* <a class="edit">Edit Profile</a> */}
-                    {/* <a class="log-out" onClick={login()}>Log In</a> */}
+                    
                     <div>
-                        <div className={success?'hide':'show'}>
-                            
-                            
                         
-                        <GoogleLogin 
-                            render={renderProps => (
-                                <a class="log-out" onClick={renderProps.onClick} disabled={renderProps.disabled}>Log In/Sign Up</a>
-                            )}
-                            clientId={clientId}
-                            buttonText="Sign in with Google"
-                            onSuccess={responseGoogle}
-                            
-                            onFailure={responseGoogle1}
-                            cookiePolicy={'single_host_origin'}
-                            isSignedIn={true}
-                        />
-                        </div>
-                        <div className={success?'show':'hide'}>
+                        <div >
                         <a class="edit">Edit Profile</a>
                         <GoogleLogout
                         render={renderProps => (
@@ -94,8 +48,8 @@ export default function Profile({isProfileClicked}) {
                         )}
                             clientId={clientId}
                             buttonText="Logout"
-                            onLogoutSuccess={() => {console.log('loggedout successfully');setsuccess(false)}}
-                            onLogoutFailure={(err) => {console.log(err);setsuccess(true)}}
+                            onLogoutSuccess={() => {console.log('loggedout successfully');auth_reset()}}
+                            onLogoutFailure={(err) => {console.log(err);auth_continue()}}
                         />
                         
                         </div>
