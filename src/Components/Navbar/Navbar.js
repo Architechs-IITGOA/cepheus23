0b94120ab9aa7f63b32d641feac8760efe061b47
adarsh_nidamanuri <99207348+Adarsh-N123@users.noreply.github.com>
2022-12-30 23:51:43 +0530

    google authentication for navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,22 @@
-import React from "react";
+// import React from "react";
+import React, { Component,useEffect,useState } from 'react'
+import { gapi } from 'gapi-script';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import "./Navbar.css"
 
-export default function Navbar({handleProfileClick}) {
+export default function Navbar({handleProfileClick,successor,auth_start,auth_failure}) {
+    const clientId = '433800136317-usopqpji8k1tu6u1pq1r2t689j091p65.apps.googleusercontent.com';
+    // const [success,setsuccess] = useState(false);
+    const responseGoogle = (response) => {
+        console.log(response);
+        auth_start();
+        // setsuccess(true);
+    }
+    const responseGoogle1 = (response) => {
+        console.log(response);
+        auth_failure();
+        // setsuccess(false);
+    }
     return (
         <div className="navbar">
             <div class="navbar-logo">
@@ -16,7 +31,26 @@ export default function Navbar({handleProfileClick}) {
                 <a href="#Schedule">Schedule</a>
                 <a href="#Sponsors">Sponsors</a>
                 <a href="#Contact">Contact</a>
-                <img src="img/user_placeholder.png" alt="avatar" onClick={() => handleProfileClick()}></img>
+                <div style={successor?{display:'none'}:{display:'block'}}>
+                <GoogleLogin 
+                            render={renderProps => (
+                                
+                                <a href='#' style={{textDecoration:'none',color:'white'}} onClick={renderProps.onClick} disabled={renderProps.disabled} >Login/SignUp</a>
+                            )}
+                            clientId={clientId}
+                            buttonText="Sign in with Google"
+                            onSuccess={responseGoogle}
+                            
+                            onFailure={responseGoogle1}
+                            cookiePolicy={'single_host_origin'}
+                            isSignedIn={true}
+                            
+                        />
+                
+                </div>
+                {/* <div style={success?{display:'block'}:{display:'none'}}> */}
+                <img style={successor?{display:'block'}:{display:'none'}} src="img/user_placeholder.png" alt="avatar" onClick={() => handleProfileClick()}></img>
+                {/* </div> */}
             </div>
         </div>
     )
