a47a2454492f6b3c291ed9d897acd230b0d66e24
Utsav Bansal <utsavbansal75@gmail.com>
2023-01-10 00:11:08 +0530

    updating about us section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,20 +4,23 @@ import React from "react";
 import './index.css';
 
 function About() {
- 
+
   return (
     <div className="aboutus" id="About">
       <center>
         <div className="myStyleh1">About Us</div>
-        <div className="blur_background"></div>
+        {/* <div className="blur_background"></div> */}
 
         <div className="text">
-            &nbsp;&nbsp;IIT Goa, only six years into its inception, has already grown to be a highly sought-after institution for engineering aspirants throughout the country, nestled into the vast and beautiful campus of GEC in Farmagudi with a vibrant population of over three thousand students and five hundred faculty members. With Cepheus, we strive to achieve excellence in technical endeavours; however, we are not untouched by the rich and unique culture of Goa, which will be reflected in the fest’s various subtleties.
-            <br></br>
-            Cepheus is a celebration of our technical prowess - not just as an institution, but as a country. It aims at instilling a sense of collective learning by being a melting pot of ideas open to students of various institutions and disciplines. With intriguing events and competitions, along with resourceful workshops by experts and professionals, we wish to make Cepheus ‘22 a memorable experience for all concerned.
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          IIT Goa, only seven years into its inception has grown to be a highly sought-after institution for engineering aspirants throughout the country, nestled into the vast and beautiful campus of GEC in Farmagudi with a vibrant population of over three thousand students and five hundred faculty members.
+          <br></br>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          With Cepheus, we strive to promote and foster technical spirit within the young and imaginative minds of students while also representing the rich and unique culture of Goa, which will be evident in the fest’s various subtleties.
+          <br></br>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          Cepheus is a celebration of our technical prowess - not just as an institution, but as a country. We aim at instilling a sense of collective learning by creating a melting pot of ideas open to students of all institutions and disciplines. With intriguing events and competitions, along with resourceful workshops by experts and professionals, we hope to make Cepheus ‘23 a memorable experience for all.
         </div>
-
-        
       </center>
     </div>
   );
