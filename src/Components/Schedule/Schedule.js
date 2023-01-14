1477defa8e8b5c200d6d191971adc10ceec60fef
Ayush Gautam <gautamayush953@gmail.com>
2023-01-15 01:25:02 +0530

    schedule update

--- src/Components/Schedule/Schedule.js
+++ src/Components/Schedule/Schedule.js
@@ -11,24 +11,22 @@ export default function Schedule() {
           <div className="scheduleCard feb8">
             <p className="schedule-date">8 February</p>
             <div className="schedule-list">
-                <ul>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
+                <ul style={{width: 'fit-content'}}>
+                  <li>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Rule The Market - 10:00 AM</li> 
+                  <li>&emsp; &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Hackoverflow - 1:00 PM</li> 
+                  <li>&emsp;&emsp;&emsp;CTF - 9:00 PM</li> 
+                  <li>&emsp; &emsp;&emsp;&emsp;&emsp;&emsp;Bridge Building - 9:00 PM</li> 
                 </ul>
             </div>
           </div>
           <div className="scheduleCard feb9">
             <p className="schedule-date">9 February</p>
             <div className="schedule-list">
-                <ul>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
+                <ul style={{width: 'fit-content'}}>
+                <li>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Circuital Dilemma - 6:00 PM</li>
+                <li>&emsp;&emsp;&emsp;&emsp;&emsp;Fizzbuzz - 7:00 PM</li>
+                <li>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Data Science Hackathon - 9:00 PM</li>
+                <li>&emsp;&emsp;&emsp;Lorem Ipsum - 9:00 PM</li>
                 </ul>
             </div>
           </div>
@@ -36,11 +34,12 @@ export default function Schedule() {
             <p className="schedule-date">10 February</p>
             <div className="schedule-list">
                 <ul>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
+                  <li>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Copy The Nature - 1st Feb</li>
+                  <li>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Scratch Competition -	10:00 AM</li>
+                  <li>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Arduino's Trial -	10:00 AM</li>
+                  <li>&emsp;&emsp;KBC Quiz - 3:00 PM</li>
+                  <li>&emsp;&emsp;&emsp;&emsp;&emsp;Online Treasure Hunt - 4:00 PM</li>
+                  <li>HacktheGames - 6:00 PM</li>
                 </ul>
             </div>
           </div>
@@ -48,11 +47,9 @@ export default function Schedule() {
             <p className="schedule-date">11 February</p>
             <div className="schedule-list">
                 <ul>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
+                  <li>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Buy My Code - 11:00 AM</li>
+                  <li>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Maze Amaze - 12:00 PM</li>
+                  <li>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Treasure Hunt - 4:00 PM</li>
                 </ul>
             </div>
           </div>
@@ -60,11 +57,10 @@ export default function Schedule() {
             <p className="schedule-date">12 February</p>
             <div className="schedule-list">
                 <ul>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
-                  <li>Bridge Builder</li>
+                  <li>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;E-Bike Competition - 9th Feb</li>
+                  <li>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Pare It Down - 11:00 AM</li>
+                  <li>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Launch Galaset - 12:00 PM</li>
+                  <li>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Game Theory Event - 3:00 PM</li>
                 </ul>
             </div>
           </div>
