a458ff0b4ba52a9cca16205e6c6fae1655a0dd49
NIDAMANURI SAI ADARSH <adarshkln5@gmail.com>
2023-01-15 16:28:32 +0530

    update

--- src/Components/Events/Events.js
+++ src/Components/Events/Events.js
@@ -882,7 +882,7 @@ class Events extends React.Component {
 					<br></br>
 					<br></br>
 					{/* <br></br> */}
-					<h1 id="main-heading">EVENTS</h1>
+					<h1 className="myStyleh1">EVENTS</h1>
 	
 					{/* <div className={clicked||clicked2||clicked3?'secondoff1':'firston1'}> */}
 					<div  className={clicked||clicked2||clicked3?'eventzoff100':'eventzon100'}>
